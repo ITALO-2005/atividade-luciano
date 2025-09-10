af0f8a8604432c8c6457ac0fcefa2df2e50d4961
ITALO-2005 <italo-santos.is@academico.ifpb.edu.br>
2025-09-10 17:04:52 +0000

    .

--- atividade-luciano/src/components/Tarefa.jsx
+++ atividade-luciano/src/components/Tarefa.jsx
@@ -3,13 +3,12 @@ import './Tarefa.css';
 
 function Tarefa({ titulo, responsavel, concluida }) {
   const cardClassName = `tarefa-card ${concluida ? 'concluida' : ''}`;
-
   return (
     <div className={cardClassName}>
       <h3>{titulo}</h3>
       <p>Responsável: {responsavel}</p>
       <p className="status">
-        Status: {concluida ? 'Concluída ✔️' : 'Pendente ⏳'}
+        Status: {concluida ? 'Concluída ' : 'Pendente '}
       </p>
     </div>
   );
